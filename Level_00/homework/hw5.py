642abf62739bf3134254ce0c34e8bc761c488498
andriamekaluashvili <159564345+andriamekaluashvili@users.noreply.github.com>
2026-04-26 00:58:06 +0400

    Delete Level_00 directory

--- Level_00/homework/hw5.py
+++
@@ -1,7 +0,0 @@
-my_name = "Nika"
-user_name = input("შეიყვანე შენი სახელი: ")
-
-if user_name == my_name:
-    print("ჩვენ გვაქვს ერთი და იგივე სახელი!")
-else:
-    print("ჩვენი სახელები განსხვავდება.")
--- Level_00/homework/hw6.py
+++
@@ -1,3 +0,0 @@
-text = input("შეიყვანე ტექსტი: ")
-for i in range(10):
-    print(text)
